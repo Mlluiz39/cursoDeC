3970d0e378395445c755063bc1ed53751702c7d2
Mlluiz39 <mlluizpereira39@gmail.com>
2021-03-08 21:04:00 -0300

    mais sobre variaveis

---
+++ aula04/variaveis2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int minhaIdade;
+    minhaIdade = 41;
+
+    int maeIdade = 73;
+
+    int irmaoIdade = 46;
+
+    printf (" Minha idade atual é %i anos.\n A idade da minha mãe é %i anos.\n A idade do meu irmão é %i anos.\n", minhaIdade, maeIdade, irmaoIdade);
+
+    return 0;
+}
