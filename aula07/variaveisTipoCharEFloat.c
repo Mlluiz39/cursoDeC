7f0b33e068ddd2842624debe36fa7a1d006d1310
Mlluiz39 <mlluizpereira39@gmail.com>
2021-03-08 21:33:03 -0300

    char float e inteiro

---
+++ aula07/variaveisTipoCharEFloat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int inteira = 10;
+
+    float decimal = 10.5;
+
+    char caractere = 'a';
+
+    char valor = '21';  // aqui vai transformar o numero em caracter
+
+    printf ("%i\n", inteira);
+    printf ("%f\n", decimal);
+    printf ("%.1f\n", decimal);
+    printf ("%c\n", caractere);
+
+    return 0;
+}
